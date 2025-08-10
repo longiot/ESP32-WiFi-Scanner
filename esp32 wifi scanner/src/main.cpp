b6e5192a0b3f86deedcedddab79a845702d80bae
Trần Long <longporo04@gmail.com>
2025-08-10 13:17:20 +0700

    initial commit

---
+++ esp32 wifi scanner/src/main.cpp
@@ -0,0 +1,34 @@
+#include <WiFi.h>
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  Serial.println("setup done. Starting WiFi scan...");
+}
+
+void loop() {
+  Serial.println("Scanning for WiFi networks...");
+  int n = WiFi.scanNetworks();
+  if (n == 0){
+    Serial.println("No networks found");
+    }
+    else {
+    Serial.print(n);
+    Serial.println(" networks found:");
+    for (int i = 0; i < n; ++i){
+      Serial.println(WiFi.SSID(i));
+      Serial.print("Signal strength: ");
+      Serial.print(WiFi.RSSI(i));
+      Serial.println(" dBm");
+      delay (100);
+    }
+  }
+  WiFi.scanDelete();
+  Serial.println("Scan complete!...");
+  delay(10000);
+  }
